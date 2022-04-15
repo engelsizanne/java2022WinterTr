7f16b5b6a4d8185db6d6f1ad4d303e89d3055a03
Seyma Olmez <engelsizanneee@gmail.com>
2022-04-15 18:07:33 +0300

    15 nisan

---
+++ src/day40_overriding_polymorphism/Dosya_deneme.java
@@ -0,0 +1,17 @@
+package day40_overriding_polymorphism;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Dosya_deneme {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String dosyaYolu="src/day40_overriding_polymorphism/dosya";
+
+        FileInputStream fis = new FileInputStream(dosyaYolu);
+
+
+
+
+    }
+}
